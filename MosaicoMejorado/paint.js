5ae058915e1c18b776a812855e5a431efdd734a3
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-01-21 14:56:09 +0100

    modifico estilos

---
+++ MosaicoMejorado/paint.js
@@ -0,0 +1,78 @@
+window.addEventListener("DOMContentLoaded", function () {
+    //primero identifico el color activo por defecto
+    let elemento = document.querySelector('#paleta .seleccionado');
+    let estilos = getComputedStyle(elemento);
+    let colorActivo = estilos.backgroundColor;
+    let pincelActivado=false;
+
+
+    dibujarTablero();
+
+    function dibujarTablero(){
+        let div = document.getElementById('zonadibujo');
+        let tabla = document.createElement('table');
+        div.appendChild(tabla);    
+        tabla.style.border = "1px solid gray";
+        for(let i=0;i<=30;i++){
+            let fila = document.createElement('tr');
+            tabla.appendChild(fila);
+            for(let j=0;j<=30;j++){
+                let celda = document.createElement('td');
+                fila.appendChild(celda);
+                celda.style.border = "1px solid gray";
+                celda.style.height = "10px";
+                celda.style.width = "10px";
+                // celda.addEventListener('mouseover');
+                // celda.style.backgroundColor="red";
+            } 
+        }
+    }
+    
+    function seleccionarColorPaleta(event){
+        let tdColorSel = document.querySelector('#paleta .seleccionado');//seleccionado que es hijo
+        tdColorSel.classList.remove("seleccionado");
+    
+        event.target.classList.add('seleccionado');
+        let estilos = getComputedStyle(event.target);
+        colorActivo = estilos.backgroundColor;
+
+    }
+
+    let tablaColores = document.getElementById("paleta");
+    let celdasColores = tablaColores.getElementsByTagName("td");
+    //pongo el mismo listener para el evento click a todas las celdas de colores de la paleta
+    for(let i=0;i<celdasColores.length;i++){
+        celdasColores[i].addEventListener('click',seleccionarColorPaleta);
+    }
+    
+    
+    function togglePincel(){//funciona como un toggle un click activa el pincel, el siguiente click lo desactiva  
+        if(pincelActivado){
+            pincelActivado=false;
+        }else{
+            pincelActivado=true;
+        }
+        //cambio el mensaje
+        if(pincelActivado){
+            document.getElementById('pincel').textContent='Pincel activado';
+        }else{
+            document.getElementById('pincel').textContent='Pincel desactivado';
+        }
+    
+    }
+
+    function pintar (event){
+        if(pincelActivado){//solo pinta si el pincel esta activado
+            event.target.style.backgroundColor = colorActivo;
+        }
+    }
+    //pongo a escuchar a todas las celdas
+    let celdas = document.querySelectorAll('#zonadibujo td');
+    for (let i=0;i<celdas.length;i++){
+        celdas[i].addEventListener('click',togglePincel);//añado el evento click para activar/desactivar el pincel
+        celdas[i].addEventListener('mouseover',pintar);//añado evento mouseover para pintar
+    }
+
+
+
+});
